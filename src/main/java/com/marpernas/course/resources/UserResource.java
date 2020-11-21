73e2f82490be5b9bb09151a0dd1af9f183937774
marpernas <marpernas@gmail.com>
2020-11-20 21:49:47 -0300

    H2 database, test profile, JPA

--- src/main/java/com/marpernas/course/resources/UserResource.java
+++ src/main/java/com/marpernas/course/resources/UserResource.java
@@ -13,7 +13,7 @@ public class UserResource {
 	
 	@GetMapping
 	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Marcos", "marpernas@gmail.com", "970115109", "12345");
+		User u = new User(1L, "Marcos", "marpernas@gmail.com", "9999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
 
