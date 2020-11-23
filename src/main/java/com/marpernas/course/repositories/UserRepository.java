b12f461bef49e41d1882ec2b4695655b50f78507
marpernas <marpernas@gmail.com>
2020-11-22 21:00:10 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/marpernas/course/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.marpernas.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.marpernas.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	
+}
