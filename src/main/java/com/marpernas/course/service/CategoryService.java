b7563a729ec5b868782bd0b38e6aa0a52181b44e
marpernas <marpernas@gmail.com>
2020-12-01 14:47:03 -0300

    Category

---
+++ src/main/java/com/marpernas/course/service/CategoryService.java
@@ -0,0 +1,27 @@
+package com.marpernas.course.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.marpernas.course.entities.Category;
+import com.marpernas.course.repositories.CategoryRepository;
+
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
